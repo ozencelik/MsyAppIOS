383c860e655827defefe226b1cb285dccfd1eacd
Özenç Çelik <zen@Ozencs-MacBook-Air.local>
2019-01-01 06:06:12 -0800

    Initial Commit

---
+++ MsyTests/MsyTests.swift
@@ -0,0 +1,34 @@
+//
+//  MsyTests.swift
+//  MsyTests
+//
+//  Created by Özenç Çelik on 1.01.2019.
+//  Copyright © 2019 Özenç Çelik. All rights reserved.
+//
+
+import XCTest
+@testable import Msy
+
+class MsyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
